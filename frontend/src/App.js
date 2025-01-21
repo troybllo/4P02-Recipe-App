084c9ee38af27c55ba3ecc576b5ebf400771112b
troybello <troybello25@gmail.com>
2025-01-21 10:38:12 -0500

    changed directory name

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1> Feastly</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
