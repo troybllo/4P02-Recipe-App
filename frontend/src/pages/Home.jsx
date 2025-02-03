4afdc83df4a89d2ad5a72c007f05dfccf3793ffa
SwaFly <canaswafly@gmail.com>
2025-02-02 23:02:59 -0500

    test commit
    added test paragraph and a button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ const Home = () => {
     <div className="p-4">
       <h1 className="text-2xl font-bold">Home Page</h1>
       <p>Welcome to the home page!</p>
+      <p>Test</p>
+      <button>Help!</button>
     </div>
   );
 };
