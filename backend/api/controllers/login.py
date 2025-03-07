386968f750916de49ded9415982e9eb25a69d549
mohamedswe <mabdelgelil74@gmail.com>
2025-03-07 06:09:06 -0500

    Clean slate

--- backend/api/controllers/login.py
+++
@@ -1,9 +0,0 @@
-from flask_jwt_extended import create_access_token
-from ..services.database_interface import get_user_by_username
-from werkzeug.security import check_password_hash
-
-def authenticate_user(username, password):
-    user = get_user_by_username(username)
-    if user and check_password_hash(user['password_hash'], password):
-        return create_access_token(identity=username), None
-    return None, "Invalid credentials"
--- backend/api/controllers/registration.py
+++
@@ -1,7 +0,0 @@
-from flask import request, jsonify
-from ..services.database_interface import add_user_to_firebase
-
-def register_user():
-    user_data = request.get_json()
-    result = add_user_to_firebase(user_data)
-    return jsonify(result)
